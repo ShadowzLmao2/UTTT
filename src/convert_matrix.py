b4aa8826ee82a0e643f6894bc22175da1a1835c6
ShadowzLmao2 <166965282+ShadowzLmao2@users.noreply.github.com>
2024-12-12 04:55:50 +0000

    Added CCW matrix flipper

--- src/convert_matrix.py
+++ src/convert_matrix.py
@@ -1,19 +1,27 @@
-def rotateMatrix(matrix):
+def rotateCWMatrix(matrix):
     for i in range(0,9):
         for j in range(0,9):
             retMatrix[i][j] = matrix[8-j][i]
     return retMatrix
+
+def rotateCCWMatrix(matrix):
+    for i in range(0,9):
+        for j in range(0,9):
+            retMatrix[i][j] = matrix[j][8 - i]
+    return retMatrix
+
 def flipUpMatrix(matrix):
     for i in range(0,9):
         for j in range(0,9):
             retMatrix[i][j] = matrix[8-i][j]
     return retMatrix
+
 def flipRightMatrix(matrix):
     for i in range(0,9):
         for j in range(0,9):
             retMatrix[i][j] = matrix[i][8-j]
-    print(retMatrix)
     return retMatrix
+
 retMatrix = (
     ([0]*9),
     ([0]*9),
