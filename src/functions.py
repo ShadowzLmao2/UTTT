091e9ba6b219e8170fc24ba063f7e4c9e3b4d934
ShadowzLmao2 <166965282+ShadowzLmao2@users.noreply.github.com>
2024-12-12 19:23:51 +0000

    Rehashed errors

--- src/functions.py
+++ src/functions.py
@@ -27,7 +27,6 @@ def drawGrid():
                 print("X", end="")
             else:
                 print("0", end="")
-            #print(grid[y][x], end="")
             if (x == 2 or x == 5) and not simplify_table:
                 print("| ", end="")
             if x < 8 and not simplify_table:
@@ -40,24 +39,14 @@ def drawGrid():
 def firstMove():
     print("x1: ", end="")
     x1 = int(input())
-    if x1 < 1 or x1 > 3:
-        print("Invalid location")
-        firstMove()
     print("y1: ", end="")
     y1 = int(input())
-    if y1 < 1 or y1 > 3:
-        print("Invalid location")
-        firstMove()
     print("x2: ", end="")
     x2 = int(input())
-    if x2 < 1 or x2 > 3:
-        print("Invalid location")
-        firstMove()
     print("y2: ", end="")
     y2 = int(input())
-    if y2 < 1 or y2 > 3:
-        print("Invalid location")
-        firstMove()
+    if y2 < 1 or y2 > 3 or x2 < 1 or x2 > 3:
+        badMove()
     pastMove[0] = (x1 - 1) * 3 + (x2 - 1)
     pastMove[1] = (y1 - 1) * 3 + (y2 - 1)
     if grid[pastMove[1]][pastMove[0]] == 0:
@@ -67,17 +56,18 @@ def firstMove():
         switchPlayer()
         drawGrid()
 
+def badMove():
+    print("Invalid location")
+    firstMove()
+
+
 def takeMove():
     print("x1: ", end="")
     xInput = int(input())
-    if xInput < 1 or xInput > 3:
-        print("Invalid location")
-        takeMove()
     print("y1: ", end="")
     yInput = int(input())
-    if yInput < 1 or yInput > 3:
-        print("Invalid location")
-        takeMove()
+    if yInput < 1 or yInput > 3 or xInput < 1 or xInput > 3:
+        badMove()
     currentMove[0] = (pastMove[0] - 1) * 3 + (xInput - 1)
     currentMove[1] = (pastMove[1] - 1) * 3 + (yInput - 1) 
     if grid[currentMove[1]][currentMove[0]] == 0:
@@ -87,8 +77,7 @@ def takeMove():
         switchPlayer()
         drawGrid()
     else:
-        print("Spot already taken")
-        takeMove()
+        badMove()
 
 def switchPlayer():
     global playerTurn
