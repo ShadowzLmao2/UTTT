6702023a0409003ed261df814176d14cdd822ae2
ShadowzLmao2 <166965282+ShadowzLmao2@users.noreply.github.com>
2025-01-08 02:53:26 +0000

    Working on solving a small game

--- src/solve_game.py
+++ src/solve_game.py
@@ -1,8 +1,15 @@
 from convert_matrix import *
 from functions import *
 from main import *
+from enum import Enum
+from standardttt import standardGrid
+class winState(Enum):
+    draw = 0
+    win  = 1
+    loss = 2
+    lose = 2
 
-def isGameEndable():
+def isGameEndable(): #for UTTT
     if countEmptySpaces() == 1:
        fillLastSpace()
     drawGrid()
@@ -10,7 +17,20 @@ def isGameEndable():
 def solveFinalBoard(): #Solves the board when the only open spaces are in one board
     return
 
-def singleTTTGameSolver():
+def singleTTTGameSolver(playerTurn):
+    if smallEmptySpaces() <= 1 or smallEmptySpaces() == 9:
+        return winState.draw
+    if winInOne():
+        return winState.win
+    if loseInOne():
+        return winState.lose
+    return
+
+def winInOne():
+    return False
+
+def loseInOne():
+    return False
 
 def countEmptySpaces():
     count = 0
@@ -18,6 +38,15 @@ def countEmptySpaces():
     for y in range(0,9):
         for x in range(0,9):
             if grid[x][y] == 0:
+                count+=1
+    return count
+
+def smallEmptySpaces():
+    count = 0
+    global standardGrid
+    for y in range(0,3):
+        for x in range(0,3):
+            if standardGrid[x][y] == 0:
                 count+=1
     return count
 
